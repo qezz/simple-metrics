ea76f98a89052fbb7086f854ebad3d93ff57a99c
Sergey Mishin <sergei.a.mishin@gmail.com>
2024-06-22 21:20:15 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,408 @@
+use regex::Regex;
+use std::{collections::BTreeMap, hash::Hash};
+
+lazy_static::lazy_static! {
+    static ref METRIC_NAME_RE: Regex = Regex::new(r"^[a-zA-Z_:][a-zA-Z0-9_:]*$").unwrap();
+    static ref LABEL_NAME_RE: Regex = Regex::new(r"^[a-zA-Z_][a-zA-Z0-9_]*$").unwrap();
+}
+
+/// Internal trait for rendering a collection of metrics into a
+/// string.
+pub trait RenderIntoMetrics {
+    fn render_into_metrics(&self) -> String;
+}
+
+/// Internal representation of sample labels
+type Labels = BTreeMap<String, String>;
+
+impl RenderIntoMetrics for Labels {
+    fn render_into_metrics(&self) -> String {
+        self.iter()
+            .map(|(k, v)| format!(r#"{}="{}""#, k, v))
+            .collect::<Vec<String>>()
+            .join(",")
+    }
+}
+
+pub fn check_labels(labels: &Labels) -> Result<(), Error> {
+    for name in labels.keys() {
+        if !LABEL_NAME_RE.is_match(name) {
+            return Err(Error::InvalidLabelName(name.to_string()));
+        }
+    }
+
+    Ok(())
+}
+
+/// A trait for rendering a Prometheus metric value into a string.
+pub trait RenderableValue {
+    fn render(&self) -> String;
+}
+
+impl RenderableValue for i64 {
+    fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+impl RenderableValue for f64 {
+    fn render(&self) -> String {
+        format!("{}", self)
+    }
+}
+
+// Booleans are rendered as 1 or 0.
+impl RenderableValue for bool {
+    fn render(&self) -> String {
+        if *self {
+            1_i64.render()
+        } else {
+            0_i64.render()
+        }
+    }
+}
+
+/// Sample holds a single measurement of metrics
+pub struct Sample {
+    labels: Labels,
+    value: Box<dyn RenderableValue>,
+}
+
+impl Sample {
+    pub fn new<T: RenderableValue + 'static>(labels: &Labels, value: T) -> Result<Self, Error> {
+        check_labels(labels)?;
+
+        Ok(Self {
+            labels: labels.clone(),
+            value: Box::new(value),
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum Error {
+    /// InvalidMetricName means the metric name doesn't comply with
+    /// the Prometheus data model
+    ///
+    /// For more details, see
+    /// https://prometheus.io/docs/concepts/data_model/
+    InvalidMetricName(String),
+
+    /// InvalidLabelName means the label name doesn't comply with the
+    /// Prometheus data model.
+    ///
+    /// For more details, see
+    /// https://prometheus.io/docs/concepts/data_model/
+    InvalidLabelName(String),
+}
+
+/// Metric type
+///
+/// This library doesn't distinguish between a counter, gauge and
+/// histogram metric types. It's your responsibility to ensure that
+/// the counter only increases and histogram provides consistent data.
+#[derive(Clone, Debug)]
+pub enum MetricType {
+    /// A counter is a cumulative metric that represents a single
+    /// monotonically increasing counter whose value can only increase
+    /// or be reset to zero on restart.
+    // TODO: Make counter work only with Option<u64>?
+    Counter,
+
+    /// A gauge is a metric that represents a single numerical value
+    /// that can arbitrarily go up and down.
+    Gauge,
+
+    /// A histogram samples observations (usually things like request
+    /// durations or response sizes) and counts them in configurable
+    /// buckets.
+    Histogram,
+}
+
+impl std::fmt::Display for MetricType {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            MetricType::Counter => write!(f, "counter"),
+            MetricType::Gauge => write!(f, "gauge"),
+            MetricType::Histogram => write!(f, "histogram"),
+        }
+    }
+}
+
+/// MetricDef represents a metric definition.
+///
+/// A metric definition consists of a name, a help string, and a
+/// metric type. The samples are dynaically added on the rendering
+/// phase.
+#[derive(Clone, Debug)]
+pub struct MetricDef {
+    name: String,
+    help: String,
+    metric_type: MetricType,
+}
+
+/// Metric definition, including its name, help string, and metric type
+impl MetricDef {
+    pub fn new(name: &str, help: &str, metric_type: MetricType) -> Result<Self, Error> {
+        if !METRIC_NAME_RE.is_match(name) {
+            return Err(Error::InvalidMetricName(name.to_string()));
+        }
+
+        Ok(Self {
+            name: name.to_string(),
+            help: help.to_string(),
+            metric_type,
+        })
+    }
+}
+
+// TODO: Replace with From<X> for MetricDef?
+pub trait ToMetricDef {
+    fn to_metric_def(&self) -> MetricDef;
+}
+
+pub struct MetricStore<K: ToMetricDef> {
+    static_labels: Labels,
+    samples: BTreeMap<K, Vec<Sample>>,
+}
+
+impl<K: ToMetricDef + Eq + PartialEq + Hash + Ord> Default for MetricStore<K> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<K: ToMetricDef + Eq + PartialEq + Hash + Ord> MetricStore<K> {
+    pub fn new() -> Self {
+        Self {
+            static_labels: Labels::new(),
+            samples: BTreeMap::new(),
+        }
+    }
+
+    /// Add static labels to MetricStore
+    ///
+    /// These labels will be added to every sample.
+    pub fn with_static_labels(self, labels: Labels) -> Self {
+        Self {
+            static_labels: labels,
+            ..self
+        }
+    }
+
+    /// Add Sample to metric
+    pub fn add_sample(&mut self, to_metric: K, sample: Sample) {
+        let v = self.samples.entry(to_metric).or_default();
+        v.push(sample);
+    }
+
+    /// Add value to metric
+    pub fn add_value<V: RenderableValue + 'static>(
+        &mut self,
+        to_metric: K,
+        labels: &Labels,
+        value: V,
+    ) -> Result<(), Error> {
+        let sample = Sample::new(labels, value)?;
+        self.add_sample(to_metric, sample);
+
+        Ok(())
+    }
+}
+
+impl<K: ToMetricDef> RenderIntoMetrics for MetricStore<K> {
+    fn render_into_metrics(&self) -> String {
+        let mut all_metrics: Vec<String> = Vec::with_capacity(self.samples.keys().len());
+
+        for (m, samples) in &self.samples {
+            let metric_def = m.to_metric_def();
+
+            let mut metrics = vec![];
+
+            for s in samples {
+                let mut labels: Labels = s.labels.clone();
+                labels.extend(self.static_labels.clone());
+
+                metrics.push(format!(
+                    "{}{{{}}} {}",
+                    metric_def.name,
+                    labels.render_into_metrics(),
+                    s.value.render()
+                ))
+            }
+
+            // TODO make sure no same labels exist?
+            // TODO make sure there are no control characters in the labels values
+
+            let rendered = format!(
+                "# HELP {} {}\n# TYPE {} {}\n{}\n",
+                metric_def.name,
+                metric_def.help,
+                metric_def.name,
+                metric_def.metric_type,
+                metrics.join("\n")
+            );
+
+            all_metrics.push(rendered);
+        }
+
+        all_metrics.join("\n")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    pub struct State {
+        name: String,
+        client: String,
+        health: bool,
+        height: i64,
+        delta: f64,
+        maybe: Option<i64>,
+    }
+
+    #[derive(Eq, Hash, PartialEq, Ord, PartialOrd)]
+    pub enum ServiceMetric {
+        WorkerHealth,
+        ServiceHeight,
+        ServiceDelta,
+        Maybe,
+    }
+
+    impl ToMetricDef for ServiceMetric {
+        fn to_metric_def(&self) -> MetricDef {
+            match self {
+                ServiceMetric::WorkerHealth => {
+                    MetricDef::new("worker_health", "worker health", MetricType::Gauge).unwrap()
+                }
+                ServiceMetric::ServiceHeight => {
+                    MetricDef::new("service_height", "service height", MetricType::Gauge).unwrap()
+                }
+                ServiceMetric::ServiceDelta => {
+                    MetricDef::new("service_delta", "service delta", MetricType::Gauge).unwrap()
+                }
+                ServiceMetric::Maybe => {
+                    MetricDef::new("service_maybe", "service maybe", MetricType::Gauge).unwrap()
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn simple() {
+        let states = vec![
+            State {
+                name: "a".into(),
+                client: "woot".into(),
+                health: true,
+                height: 100,
+                delta: 1.0,
+                maybe: Some(100),
+            },
+            State {
+                name: "b".into(),
+                client: "woot".into(),
+                health: true,
+                height: 200,
+                delta: 2.2,
+                maybe: Some(100),
+            },
+            State {
+                name: "c".into(),
+                client: "meh".into(),
+                health: true,
+                height: 300,
+                delta: 3.0,
+                maybe: Some(100),
+            },
+            State {
+                name: "d".into(),
+                client: "meh".into(),
+                health: false,
+                height: 0,
+                delta: 291283791287391287391.123,
+                maybe: None,
+            },
+        ];
+
+        let mut static_labels = BTreeMap::new();
+        static_labels.insert("process".into(), "simple-metrics".into());
+
+        let mut store: MetricStore<ServiceMetric> =
+            MetricStore::new().with_static_labels(static_labels);
+
+        for s in states {
+            let mut common = Labels::new();
+            common.insert("name".to_string(), s.name);
+
+            store.add_sample(
+                ServiceMetric::WorkerHealth,
+                Sample::new(&common, s.health).unwrap(),
+            );
+
+            let mut lbs = Labels::new();
+            lbs.insert("client".to_string(), s.client.clone());
+            lbs.extend(common.clone());
+
+            store
+                .add_value(ServiceMetric::ServiceHeight, &lbs, s.height)
+                .expect("valid");
+
+            if let Some(maybe) = s.maybe {
+                store
+                    .add_value(ServiceMetric::Maybe, &lbs, maybe)
+                    .expect("valid");
+            }
+
+            let mut lbs_p = lbs.clone();
+            lbs_p.insert("type".into(), "pos".into());
+
+            store
+                .add_value(ServiceMetric::ServiceDelta, &lbs_p, s.delta)
+                .expect("valid");
+
+            let mut lbs_n = lbs.clone();
+            lbs_n.insert("type".into(), "neg".into());
+
+            store
+                .add_value(ServiceMetric::ServiceDelta, &lbs_n, -s.delta)
+                .expect("valid");
+        }
+
+        let actual = store.render_into_metrics();
+        let expected = r#"# HELP worker_health worker health
+# TYPE worker_health gauge
+worker_health{name="a",process="simple-metrics"} 1
+worker_health{name="b",process="simple-metrics"} 1
+worker_health{name="c",process="simple-metrics"} 1
+worker_health{name="d",process="simple-metrics"} 0
+
+# HELP service_height service height
+# TYPE service_height gauge
+service_height{client="woot",name="a",process="simple-metrics"} 100
+service_height{client="woot",name="b",process="simple-metrics"} 200
+service_height{client="meh",name="c",process="simple-metrics"} 300
+service_height{client="meh",name="d",process="simple-metrics"} 0
+
+# HELP service_delta service delta
+# TYPE service_delta gauge
+service_delta{client="woot",name="a",process="simple-metrics",type="pos"} 1
+service_delta{client="woot",name="a",process="simple-metrics",type="neg"} -1
+service_delta{client="woot",name="b",process="simple-metrics",type="pos"} 2.2
+service_delta{client="woot",name="b",process="simple-metrics",type="neg"} -2.2
+service_delta{client="meh",name="c",process="simple-metrics",type="pos"} 3
+service_delta{client="meh",name="c",process="simple-metrics",type="neg"} -3
+service_delta{client="meh",name="d",process="simple-metrics",type="pos"} 291283791287391300000
+service_delta{client="meh",name="d",process="simple-metrics",type="neg"} -291283791287391300000
+
+# HELP service_maybe service maybe
+# TYPE service_maybe gauge
+service_maybe{client="woot",name="a",process="simple-metrics"} 100
+service_maybe{client="woot",name="b",process="simple-metrics"} 100
+service_maybe{client="meh",name="c",process="simple-metrics"} 100
+"#;
+        assert_eq!(actual, expected);
+    }
+}
