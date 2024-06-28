92f18c8b2e047d0b40519d4b5e1625d7d3373703
Sergey Mishin <sergei.a.mishin@gmail.com>
2024-06-28 17:45:18 +0200

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  rust_overlay = import (builtins.fetchTarball
+    "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+
+  rustVersion = "1.78.0";
+
+  rust = pkgs.rust-bin.stable.${rustVersion}.default.override {
+    extensions = [ "rust-src" "rust-analyzer" ];
+  };
+in pkgs.mkShell rec {
+  buildInputs = [ rust ];
+
+  RUST_BACKTRACE = 1;
+}
