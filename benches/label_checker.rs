7794dc337e1a731011f8ebf78ac557678b7ec9f6
Sergey Mishin <sergei.a.mishin@gmail.com>
2025-02-26 00:20:43 +0100

    wip

--- benches/label_checker.rs
+++ benches/label_checker.rs
@@ -1,6 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use simple_metrics::{
-    matchers::{NaiveLabelNameChecker, RegexLabelNameChecker},
+    matchers::{
+        NaiveLabelNameChecker, NaiveLabelNameCheckerV2, NaiveLabelNameCheckerV3,
+        NaiveLabelNameCheckerV4, RegexLabelNameChecker,
+    },
     Labels,
 };
 
@@ -26,15 +29,40 @@ fn bench_label_names(c: &mut Criterion) {
         let orig = gen_tuples(*case);
         let tuples: Vec<(&str, &str)> = orig.iter().map(|x| (x.0.as_str(), x.1.as_str())).collect();
 
-        let labels_regex = Labels::<RegexLabelNameChecker>::from(tuples.as_slice());
-        group.bench_with_input(BenchmarkId::new("regex", case), case, |b, _| {
-            b.iter(|| black_box(labels_regex.check_names()))
-        });
+        {
+            let labels_regex = Labels::<RegexLabelNameChecker>::from(tuples.as_slice());
+            group.bench_with_input(BenchmarkId::new("regex", case), case, |b, _| {
+                b.iter(|| black_box(labels_regex.check_names()))
+            });
+        }
 
-        let labels_naive = Labels::<NaiveLabelNameChecker>::from(tuples.as_slice());
-        group.bench_with_input(BenchmarkId::new("naive", case), case, |b, _| {
-            b.iter(|| black_box(labels_naive.check_names()))
-        });
+        {
+            let labels_naive = Labels::<NaiveLabelNameChecker>::from(tuples.as_slice());
+            group.bench_with_input(BenchmarkId::new("naive", case), case, |b, _| {
+                b.iter(|| black_box(labels_naive.check_names()))
+            });
+        }
+
+        {
+            let labels_naive_v2 = Labels::<NaiveLabelNameCheckerV2>::from(tuples.as_slice());
+            group.bench_with_input(BenchmarkId::new("naive_v2", case), case, |b, _| {
+                b.iter(|| black_box(labels_naive_v2.check_names()))
+            });
+        }
+
+        {
+            let labels = Labels::<NaiveLabelNameCheckerV3>::from(tuples.as_slice());
+            group.bench_with_input(BenchmarkId::new("naive_v3", case), case, |b, _| {
+                b.iter(|| black_box(labels.check_names()))
+            });
+        }
+
+        {
+            let labels = Labels::<NaiveLabelNameCheckerV4>::from(tuples.as_slice());
+            group.bench_with_input(BenchmarkId::new("naive_v4", case), case, |b, _| {
+                b.iter(|| black_box(labels.check_names()))
+            });
+        }
     }
 
     group.finish();
--- src/matchers.rs
+++ src/matchers.rs
@@ -44,6 +44,27 @@ pub fn is_valid_label_name(s: &str) -> bool {
     })
 }
 
+#[inline(always)]
+pub fn is_valid_label_name_v2(s: &str) -> bool {
+    let bytes = s.as_bytes();
+
+    if let Some(&first) = bytes.first() {
+        if !((first >= b'A' && first <= b'Z') || (first >= b'a' && first <= b'z') || first == b'_')
+        {
+            return false;
+        }
+
+        bytes.iter().skip(1).all(|&b| {
+            (b >= b'A' && b <= b'Z')
+                || (b >= b'a' && b <= b'z')
+                || (b >= b'0' && b <= b'9')
+                || b == b'_'
+        })
+    } else {
+        false
+    }
+}
+
 pub trait MetricNameChecker {
     fn is_valid(name: &str) -> bool;
 }
@@ -91,5 +112,69 @@ impl LabelNameChecker for NaiveLabelNameChecker {
 
     fn is_valid(&self, name: &str) -> bool {
         is_valid_label_name(name)
+    }
+}
+
+pub struct NaiveLabelNameCheckerV2;
+
+impl LabelNameChecker for NaiveLabelNameCheckerV2 {
+    fn init() -> Self {
+        Self {}
+    }
+
+    fn is_valid(&self, name: &str) -> bool {
+        is_valid_label_name_v2(name)
+    }
+}
+
+pub struct NaiveLabelNameCheckerV3;
+
+impl LabelNameChecker for NaiveLabelNameCheckerV3 {
+    fn init() -> Self {
+        Self {}
+    }
+
+    #[inline(always)]
+    fn is_valid(&self, s: &str) -> bool {
+        let bytes = s.as_bytes();
+
+        if let Some(&first) = bytes.first() {
+            if !(first.is_ascii_uppercase() || first.is_ascii_lowercase() || first == b'_') {
+                return false;
+            }
+
+            bytes.iter().skip(1).all(|&b| {
+                b.is_ascii_uppercase() || b.is_ascii_lowercase() || b.is_ascii_digit() || b == b'_'
+            })
+        } else {
+            false
+        }
+    }
+}
+
+pub struct NaiveLabelNameCheckerV4;
+
+impl LabelNameChecker for NaiveLabelNameCheckerV4 {
+    fn init() -> Self {
+        Self {}
+    }
+
+    #[inline(always)]
+    fn is_valid(&self, s: &str) -> bool {
+        let bytes = s.as_bytes();
+        if bytes.is_empty() {
+            return false;
+        }
+
+        match bytes[0] {
+            b'A'..=b'Z' | b'a'..=b'z' | b'_' => {}
+            _ => return false,
+        }
+
+        bytes[1..].iter().all(|&b| {
+            matches!(b,
+                b'A'..=b'Z' | b'a'..=b'z' | b'0'..=b'9' | b'_'
+            )
+        })
     }
 }
